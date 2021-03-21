d034568a3817c7edb55e58543bb4eaf3c7cfa32d
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-03-21 13:12:43 +0530

    Initial Commit

---
+++ Recipe App UI/Recipe_App_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_App_UIApp.swift
+//  Recipe App UI
+//
+//  Created by Mahesh Prasad on 21/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_App_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
